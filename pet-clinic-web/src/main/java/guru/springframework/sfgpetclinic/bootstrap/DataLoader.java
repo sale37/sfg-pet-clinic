31530bb657014c854751935594252efc54898daf
Sasa Ilic <sale-37@live.com>
2018-10-22 15:45:36 +0200

    Adding static resources. closses #16

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+    }
+
+}
